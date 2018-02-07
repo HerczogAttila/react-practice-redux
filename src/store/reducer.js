7d3a87f5984b62c01fefb36a3c844912eed4c13d
Herczog Attila <aherczog@precognox.com>
2018-02-07 12:49:17 +0100

    245. Updating State Immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,36 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return {
-        counter: state.counter + 1
-      };
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
     case 'DECREMENT':
       return {
-        counter: state.counter + 1
+        ...state,
+        counter: state.counter - 1
       };
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.value
       };
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.value
       };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter})
+      };
+    case 'DELETE_RESULT':
+      break;
   }
 
   return state;
